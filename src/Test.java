f026cac3a7ec6291fe4147c068c6ea2c104fa347
aagrawa7 <aagrawa7@ncsu.edu>
2016-03-26 14:29:18 -0400

    test2

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test file to make sure everything works.");
+		System.out.println("test 2");
 	}
-
 }
