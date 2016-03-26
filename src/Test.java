a2f69a4ed5ef949fc3f36e59302137109f9e35f4
jzavala287 <jzavala287@gmail.com>
2016-03-26 14:51:39 -0400

    update 3

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("test file to make sure everything works.");
 		String name = "Test 2 string";
+		String name2 = "Test 3 String";
 	}
 
 }
